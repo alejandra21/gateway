c9569a8317e5e88456eec0a3730751f97f9e308f
Alejandra Cordero <alejandra.corderogarcia21@gmail.com>
2020-10-01 12:36:53 +0200

    -Swagger included

--- src/main/java/com/example/gatewayservice/security/SecurityTokenConfig.java
+++ src/main/java/com/example/gatewayservice/security/SecurityTokenConfig.java
@@ -32,16 +32,23 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
   	protected void configure(HttpSecurity http) throws Exception {
     	   http
 		.csrf().disable()
-		    // make sure we use stateless session; session won't be used to store user's state.
 	 	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) 	
 		.and()
-		    // handle an authorized attempts 
 		    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)) 	
 		.and()
-		   // Add a filter to validate the tokens with every request
 		.addFilterAfter(new JwtTokenAuthenticationFilter(jwtHeader,jwtPrefix,secretKey), UsernamePasswordAuthenticationFilter.class)
 		.authorizeRequests()
-		   .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+		   .antMatchers(
+				   "/auth/**",
+				   "/error",
+                   "/v2-docs",
+                   "/itinerary-api/v2/api-docs",
+					"/v2/api-docs",
+                   "/swagger-ui.html",
+                   "/webjars/**",
+                   "/swagger-resources/configuration/ui", 
+                   "/swagger-resources/configuration/security",
+                   "/swagger-resources/**").permitAll()
 		   .anyRequest().authenticated(); 
 	}
 
